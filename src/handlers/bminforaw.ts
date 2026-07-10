4d74425db8978b96cae318c4bc31b4d0162d5356
bas1licate <139423631+bas1licate@users.noreply.github.com>
2026-07-10 01:20:38 -0700

    i think this looks nicer

--- src/handlers/bminforaw.ts
+++ src/handlers/bminforaw.ts
@@ -31,28 +31,29 @@ class bminforawHandler implements FormatHandler {
     outputFormat: FileFormat
   ): Promise<FileData[]> {
     // once upon a time, there was a boy
-    const nasty = ((mean) => [mean&65535,mean>>16&65535])
+    const nasty = ((mean) => new UInt16Array([mean,mean>>16]))
     const toui8 = ((base) => new Uint8Array(base.buffer))
     const outputFiles: FileData[] = [];
     for (const File of inputFiles) {
       let bytes = new Uint8Array(file.bytes);
-      let isize = bytes.reverse().byteLength; let bd;
-      if (isize%4==0) {bd = 4}
-      else {switch (0) {
+      let isize = bytes.reverse().byteLength; let bd,c;
+      switch (0) {
         case isize%3: bd=3;break
         case isize%2: bd=2;break
         default: bd = 1; var c = new Uint8Array(1024); for (let i = 0; i < 256; i++) {c.set([i,i,i,0],4*i)}}
-            const k=(bd==3?12:4); var bpr = k*Math.ceil(isize/k)-isize}
-      const p = new Uint8Array(bpr ?? 0); const pp = p.byteLength; const cc = c.?byteLength ?? 0
-      function getdivs(k) {const res = []; for (let i = 2; i <= sqrt(k); i++) {if (k%i==0){res.push(k)}}; return res}
+      const k=(bd==3?12:4); const bpr = k*Math.ceil(isize/k)-isize
+      const p = new Uint8Array(bpr); const pp = p.byteLength; const cc = c.?byteLength ?? 0;
+      try {let d = [...c]}
+      catch (e) {console.log("caught",e); var c = new Uint8Array(0)}
+      function getdivs(k) {const res = []; for (let i = 2; i <= sqrt(k); i++) {if (k%i==0) {res.push(k)}}; return res}
       const ks = isize/bd; const hd = getdivs(ks).map((x) => isize/x); const dim = [...nasty(hd[hd.length-1]),...nasty(ks/hd[hd.length-1])]
       isize += pp; const o = 54+cc; const fs = isize+o;
-      const Header1 = new Uint16Array([28002,...nasty(fs),0,0,54+o,0]);
+      const Header1 = new Uint16Array([0x424D,...nasty(fs),0,0,54+o,0]);
       const Header2 = new Uint16Array([40,0,...dim,1,bd*8,0,0,...nasty(isize),2835,0,2835,0,0,0,0])
       // should mention dimensions are/were defined semi-arbitrarily due to operating from raw rgb
       const full = new Uint8Array(fs)
       full.set(toui8(Header1),0); full.set(toui8(Header2),10);
-      full.set(tou18(c??new Int8Array(0)),54)
+      full.set(c,54)
       full.set(p,o); full.set(bytes,pp+o)
       outputFiles.push(name: file.name.split(".").slice(0, -1).join(".") + ".bmp", bytes: full)
     }
