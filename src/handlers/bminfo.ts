d73ec12b399a19e9a9d0e3c9cdbca61b537e9a30
bas1licate <139423631+bas1licate@users.noreply.github.com>
2026-07-09 20:45:58 -0700

    header init

--- src/handlers/bminfo.ts
+++ src/handlers/bminfo.ts
@@ -31,8 +31,12 @@ class bminfoHandler implements FormatHandler {
     outputFormat: FileFormat
   ): Promise<FileData[]> {
     // once upon a time, there was a boy
-    // i'll add the code once i figure out how to read the data of the input
     const outputFiles: FileData[] = [];
+    const Header1: new Uint16Array([28002,-1,0,0,54,0]);
+    // total file size (calculated last): Header1[1]
+    const Header2: new Uint16Array([40,0,-1,-2,-1,-2,1,32,0,0,0,0,2835,0,2835,0,2835,0,0,0,0,0])
+    // dimensions: Header2[2:4]*Header[4:6] = Header[8:9]
+    
     return outputFiles;
   }
 
