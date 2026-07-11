2602d0eeac72781915c4ca122c353ba2633d2b99
bas1licate <139423631+bas1licate@users.noreply.github.com>
2026-07-11 13:35:48 -0700

    and no more of that

--- src/handlers/bminforaw.ts
+++ src/handlers/bminforaw.ts
@@ -59,7 +59,7 @@ class bminforawHandler implements FormatHandler {
       const full = new Uint8Array(fs)
       full.set(toui8(Header1),0); full.set(toui8(Header2),14);
       full.set(c,54)
-      for (let i = o, j = 0, k=0,row = new Array(n),q,rrow,ow; k<dim[1]; i+=dim[0]+bpr,j+=dim[0],k++)
+      for (let i = o, j = 0, k=0,row = new Array(n),rrow,ow; k<dim[1]; i+=dim[0]+bpr,j+=dim[0],k++)
       {rrow = bytes.slice(j,j+dim[0]); for (let z = 0; z < dim[0]; z+=3) {row[z/3] = rrow.slice(z,z+3)}; ow = new Uint8Array(...row.reverse());
        full.set(ow,i); full.set(p,dim[0]+i)}
       outputFiles.push(name: file.name.split(".").slice(0, -1).join(".") + ".bmp", bytes: full)
