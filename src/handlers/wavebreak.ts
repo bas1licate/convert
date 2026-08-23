0d1b70da8b1dee4023f9bd87f87727c98b986e42
bas1licate <139423631+bas1licate@users.noreply.github.com>
2026-08-22 21:53:10 -0700

    one more thing actually - adding padding

--- src/handlers/wavebreak.ts
+++ src/handlers/wavebreak.ts
@@ -30,7 +30,7 @@ class wavebreakHandler implements FormatHandler {
     const head1 = new Uint16Array([18770,17990,...split32(sz+36),16727,17750])
     const head2 = new Uint16Array([28006,8308,16,0,1,1,44100,0,22664,1,2,16])
     const head3 = new Uint16Array([24932,24948,...split32(sz)])
-    const r = new Uint8Array(sz+44);
+    const r = new Uint8Array(2*Math.ceil(sz/2)+44);
     r.set(to8(head1),0);r.set(to8(head2),12);r.set(to8(head3),36)
     r.set(file.bytes,44)
     outputFiles.push({name: file.name.split(".").slice(0, -1).join(".") + ".wav", bytes: r})
