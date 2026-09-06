bb44cba3e12a7838b2bcead6d71f281ef7dbee5c
bas1licate <139423631+bas1licate@users.noreply.github.com>
2026-09-06 15:52:41 -0700

    not even gonna pretend the previous case worked

--- src/handlers/wavebreak.ts
+++ src/handlers/wavebreak.ts
@@ -28,14 +28,14 @@ class wavebreakHandler implements FormatHandler {
     outputFormat: FileFormat
   ): Promise<FileData[]> {
     const outputFiles: FileData[] = [];
-    const to8 = (t : Uint16Array | Uint32Array ) => new Uint8Array(t.buffer)
+    const n32 = (t : number) => new Uint8Array(new Uint32Array([t]).buffer)
     for (const file of inputFiles) {
     if (file.bytes.byteLength > 0xFFFFFF00) {console.error("data too large. maximum size 4,294,967,040 bytes."); continue}
     if (file.bytes.byteLength > 0x7FFFFF00) {console.warn("data very large. successful conversion cannot be guaranteed.")}
     const sz = 2*Math.ceil(file.bytes.byteLength/2)
-    const head1 = new Uint8Array([82,73,70,70,...to8(sz+36),87,65,86,69])
+    const head1 = new Uint8Array([82,73,70,70,...n32(sz+36),87,65,86,69])
     const head2 = new Uint8Array([102,109,116,32,16,0,0,0,1,0,1,0,68,172,0,0,136,88,1,0,2,0,16,0])
-    const head3 = new Uint8Array([100,97,116,97,...to8(sz)])
+    const head3 = new Uint8Array([100,97,116,97,...n32(sz)])
     const r = new Uint8Array(sz+44);
     r.set(head1,0);r.set(head2,12);r.set(head3,36)
     r.set(file.bytes,44)
